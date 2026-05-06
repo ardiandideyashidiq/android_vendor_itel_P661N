8dea985bf12a1db997a5b36a5da3ee4d1790d4d9
Your Name <you@example.com>
2026-05-06 09:38:08 +0000

    init

---
+++ Android.mk
@@ -0,0 +1,22 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),P661N)
+
+$(call add-radio-file-sha1-checked,radio/dpm.img,ef9ad6c0ed7ea1b40614ee7d41cea06b78b8a978)
+$(call add-radio-file-sha1-checked,radio/gz.img,1be54ebb8b661532d4e5b1a4d409e7fcba6b42d8)
+$(call add-radio-file-sha1-checked,radio/lk.img,040bb0ffaac1a61b8d729f456834f698aae785a7)
+$(call add-radio-file-sha1-checked,radio/logo.img,a4009108ffd2c3b2e7dcb15bb5fff5007f3eb1e7)
+$(call add-radio-file-sha1-checked,radio/mcupm.img,fdd973914726725ba19465d999ad8a9e868a018a)
+$(call add-radio-file-sha1-checked,radio/md1img.img,f8efbd0770a02329979fed2db68487d7928e6f86)
+$(call add-radio-file-sha1-checked,radio/pi_img.img,2533db91b54d5fd7d21e2be2f4c4fe66ff1fe15b)
+$(call add-radio-file-sha1-checked,radio/scp.img,8ba9f52aa6342707622adb138a35397d098e4fab)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,a36cd19bca5dafdf3760dd492c3d489a4757cb09)
+$(call add-radio-file-sha1-checked,radio/sspm.img,193776e8c9985a0d019040f0293f1a7428339cd8)
+$(call add-radio-file-sha1-checked,radio/tee.img,c0cd11347863fa1fd24094a7c1ac0ca596118349)
+$(call add-radio-file-sha1-checked,radio/tkv.img,9b7c910dff0e623ed64a25febcd960fdf41d828d)
+
+endif
